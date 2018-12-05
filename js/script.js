179a563b3963bb471cd4388e4d37069bff46d1e8
Gabriel Santiago <gisantiago@live.com>
2018-12-05 10:09:15 -0500

    Added functions

--- js/script.js
+++ js/script.js
@@ -16,8 +16,17 @@ FSJS project 2 - List Filter and Pagination
    will only be used inside of a function, then it can be locally 
    scoped to that function.
 ***/
+const listItems = document.querySelectorAll('.student-list li');
+let pagination = document.createElement('div');
+pagination.className = 'pagination';
+document.querySelector('.page').appendChild(pagination);
+let ul = document.createElement('ul');
+document.querySelector('.pagination').appendChild(ul);
 
 
+let pageSize = 10;
+let pageCount = Math.ceil(listItems.length / pageSize);
+let startPage = 1;
 
 
 /*** 
@@ -34,7 +43,17 @@ FSJS project 2 - List Filter and Pagination
        that will be passed into the parens later when you call or 
        "invoke" the function 
 ***/
-
+function showPage(list, page = 1) {
+   for (let i = 0; i < list.length; i++) {
+      if (i >= (page * pageSize) - pageSize && i <= (((page * pageSize) - pageSize) + pageSize) - 1) {
+         list[i].style.display = 'block';
+      } else {
+         list[i].style.display = 'none';
+      }
+   }
+   return list;
+}
+showPage(listItems, startPage);
 
 
 
@@ -43,6 +62,29 @@ FSJS project 2 - List Filter and Pagination
    functionality to the pagination buttons.
 ***/
 
+function appendPageLinks(list) {
+
+   for (let i = 1; i <= pageCount; i++) {
+      let li = document.createElement('li');
+      document.querySelector('.pagination ul').appendChild(li);
+      let a = document.createElement('a');
+      document.querySelector('.pagination ul li').appendChild(a);
+      a.setAttribute('href', '#');
+      a.innerHTML = i;
+      a.addEventListener('click', (e) => {
+         const currentLink = e.target.textContent;
+         showPage(listItems, currentLink);
+         links = document.querySelectorAll('.active');
+         for (let i = 0; i < links.length; i++) {
+            links[i].classList.remove('active');
+         }
+         e.target.className = 'active';
+      });
+   }
+   
+}
+
+appendPageLinks(listItems);
 
 
 
