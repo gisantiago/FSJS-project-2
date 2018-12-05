7e537e177b685412185035b564eebeb6b1e9459c
Gabriel Santiago <gisantiago@live.com>
2018-12-05 10:34:05 -0500

    Added comments

--- js/script.js
+++ js/script.js
@@ -1,20 +1,11 @@
 /******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
+ist Filter and Pagination
 ******************************************/
    
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
 /*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
+   Global variables... DOM selectors
 ***/
 const listItems = document.querySelectorAll('.student-list li');
 let pagination = document.createElement('div');
@@ -30,18 +21,9 @@ let startPage = 1;
 
 
 /*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
+   The `showPage` function is used to show the number of items allowed on the `pageSize` 
+   variable and hide all other item in the list. 
+   On this case the default pageSize is set to (10).
 ***/
 function showPage(list, page = 1) {
    for (let i = 0; i < list.length; i++) {
@@ -58,8 +40,9 @@ showPage(listItems, startPage);
 
 
 /*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
+   The `appendPageLinks function generates/appends HTML elements...
+   This is where the pagination links are dynamically created and attached to the index.HTML.
+   this function also call the `showPage` function every time the link buttons are clicked.  
 ***/
 
 function appendPageLinks(list) {
@@ -85,8 +68,3 @@ function appendPageLinks(list) {
 }
 
 appendPageLinks(listItems);
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
